ba190bd828786e8c5502320cbe3f81d79cd5515c
AlexeykaVas <107036801+AlexeykaVas@users.noreply.github.com>
2023-08-24 15:05:51 +0300

    Delete db-auto-compiler directory

--- db-auto-compiler/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-sheet_name = "Контакты учеников"
-skiprows = None
-header = 0
-names = None
-usecols = ["Класс", "Фамилия", "Имя", "Отчество"]
-dtype = None
-df = pd.read_excel("C:/Users/alexe/Desktop/Alexey/Projects/project-summer-2023/db-auto-compiler/Силаэдр 2022-23.xlsx")
-print(df)
