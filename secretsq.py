112e9073d8e098bfeee8dc3deaa0ddff460e0ac6
AlexeykaVas <107036801+AlexeykaVas@users.noreply.github.com>
2023-08-29 22:29:49 +0300

    _style

--- secretsq.py
+++ secretsq.py
@@ -2,4 +2,4 @@ import hashlib
 secret_cookie = '9937frrr875successfullyjrhxnuon?u)359w_8y5h_q8p95y36'
 pass1 = 'd033e22ae348aeb5660fc2140aec35850c4da997'
 admin_key = '11'
-ip_addresses = ["127.0.0.1"]
+ip_addresses = ["127.0.0.1", "192.168.1.181"]
